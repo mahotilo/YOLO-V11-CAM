56e630a0f26611589558ac4ef03281c37ebfd447
mahotilo <kvmahotilo@gmail.com>
2026-05-20 13:18:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='yolo_cam',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(include=['yolo_cam', 'yolo_cam.*']),
     install_requires=[
         'ttach'
     ],
