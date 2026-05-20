63879094f91f3e0e7068ed40cd5f2849cc42dbba
mahotilo <kvmahotilo@gmail.com>
2026-05-20 11:35:08 +0300

    Update activations_and_gradients.py

--- yolo_cam/activations_and_gradients.py
+++ yolo_cam/activations_and_gradients.py
@@ -17,17 +17,14 @@ class ActivationsAndGradients:
                 target_layer.register_forward_hook(self.save_gradient))
 
     def save_activation(self, module, input, output):
-        #activation = output
-        activation = output[0] if isinstance(output, tuple) else output
-
+        activation = output
+        
         if self.reshape_transform is not None:
             activation = self.reshape_transform(activation)
         #self.activations.append(activation.cpu().detach())
         self.activations.append(activation.cpu().detach().numpy())
 
     def save_gradient(self, module, input, output):
-        if isinstance(output, tuple):
-            output = output[0]        
         if not hasattr(output, "requires_grad") or not output.requires_grad:
             # You can only register hooks on tensor requires grad.
             return
