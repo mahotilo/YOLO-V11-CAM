de843be49e59218626d6f73785c483f36e1606ec
mahotilo <kvmahotilo@gmail.com>
2026-05-20 12:04:03 +0300

    Update activations_and_gradients.py

--- yolo_cam/activations_and_gradients.py
+++ yolo_cam/activations_and_gradients.py
@@ -18,11 +18,10 @@ class ActivationsAndGradients:
 
     def save_activation(self, module, input, output):
         activation = output
-        
+
         if self.reshape_transform is not None:
             activation = self.reshape_transform(activation)
-        #self.activations.append(activation.cpu().detach())
-        self.activations.append(activation.cpu().detach().numpy())
+        self.activations.append(activation.cpu().detach())
 
     def save_gradient(self, module, input, output):
         if not hasattr(output, "requires_grad") or not output.requires_grad:
@@ -45,4 +44,3 @@ class ActivationsAndGradients:
     def release(self):
         for handle in self.handles:
             handle.remove()
-
